cc3fa2fb18c52f3b773c99683c430d4d6d68d5a0
Andrea Carapelli <carapelliandrea@gmail.com>
2024-02-02 11:35:57 +0100

    add FloatingLeg utilities

--- instruments/swap.py
+++ instruments/swap.py
@@ -1,4 +1,4 @@
-from index.curverateindex import CurveRateIndex, Schedule
+from index.curverateindex import IborIndex, Schedule
 from interface.floatingleg import *
 from interface.fixedleg import *
 from interface.trade import Trade
@@ -7,23 +7,23 @@ class SwapDirection:
     PAYER = 1
     RECEIVER = -1
 
-class SwapFixedFloating(Trade):
-    def __init__(self, 
-                float_schedule: Schedule,
-                fix_schedule: Schedule,
-                float_notionals: list[float],
-                fix_notionals: list[float],
-                gearings: list[float],
-                spreads: list[float],
-                index: CurveRateIndex,
-                fix_coupons
-                ) -> None:
-        super().__init__()
-        self.floating_leg = FloatingRateLeg(float_schedule, float_notionals, gearings, spreads, index)
-        self.fixed_leg = FixedRateLeg(fix_schedule, fix_notionals, fix_coupons)
+# class SwapFixedFloating(Trade):
+#     def __init__(self, 
+#                 float_schedule: Schedule,
+#                 fix_schedule: Schedule,
+#                 float_notionals: list[float],
+#                 fix_notionals: list[float],
+#                 gearings: list[float],
+#                 spreads: list[float],
+#                 index: CurveRateIndex,
+#                 fix_coupons
+#                 ) -> None:
+#         super().__init__()
+#         self.floating_leg = FloatingRateLeg(float_schedule, float_notionals, gearings, spreads, index)
+#         self.fixed_leg = FixedRateLeg(fix_schedule, fix_notionals, fix_coupons)
 
-    def price(self, discount_curve, evaluation_date: datetime):
-        return self.floating_leg.npv(discount_curve, evaluation_date) + self.fixed_leg.npv(discount_curve, evaluation_date)   
+#     def price(self, discount_curve, evaluation_date: datetime):
+#         return self.floating_leg.npv(discount_curve, evaluation_date) + self.fixed_leg.npv(discount_curve, evaluation_date)   
         
     
 
@@ -35,11 +35,11 @@ class SwapFixedFloating_dev(Trade):
                 fix_notionals: list[float],
                 gearings: list[float],
                 spreads: list[float],
-                index: CurveRateIndex,
+                index: IborIndex,
                 fix_coupons
                 ) -> None:
         super().__init__()
-        self.floating_leg = FloatingRateLeg_dev(float_schedule, float_notionals, gearings, spreads, index)
+        self.floating_leg = FloatingRateLeg(float_schedule, float_notionals, gearings, spreads, index)
         self.fixed_leg = FixedRateLeg(fix_schedule, fix_notionals, fix_coupons)
 
     def price(self, discount_curve, term_structure, evaluation_date: datetime):
--- interface/floatingleg.py
+++ interface/floatingleg.py
@@ -2,179 +2,180 @@ from datetime import datetime, timedelta
 from interface.coupon import *
 from index.curverateindex import * 
 from utilities.time import Settings
+import pandas as pd
  
-class FloatingCoupon(Coupon):
-    ''' 
-    Concrete Floating Coupon object. 
-    '''
-    def __init__(self,
-                 payment_date: datetime,
-                 nominal: float,
-                 accrual_start_date: datetime,
-                 accrual_end_date: datetime,
-                 index: CurveRateIndex, 
-                 gearing: float, # multiplicative coefficient of the index fixing
-                 spread: float, # fixed spread
-                 ref_period_start: datetime,
-                 ref_period_end: datetime,
-                 daycounter: DayCounter,
-                 ex_coupon_date: datetime,
-                 is_in_arrears: bool = False, # da implementare convexity adjustments
-                 fixing_days: int = None,
-                 ):
-        super().__init__(payment_date, nominal, accrual_start_date, accrual_end_date, ref_period_start, ref_period_end, ex_coupon_date)
-        self._day_counter = daycounter
-        self._fixing_days = fixing_days 
-        self._index = index
-        self._gearing = gearing
-        self._spread = spread
-        self._is_in_arrears = is_in_arrears
+# class FloatingCoupon(Coupon):
+#     ''' 
+#     Concrete Floating Coupon object. 
+#     '''
+#     def __init__(self,
+#                  payment_date: datetime,
+#                  nominal: float,
+#                  accrual_start_date: datetime,
+#                  accrual_end_date: datetime,
+#                  index: CurveRateIndex, 
+#                  gearing: float, # multiplicative coefficient of the index fixing
+#                  spread: float, # fixed spread
+#                  ref_period_start: datetime,
+#                  ref_period_end: datetime,
+#                  daycounter: DayCounter,
+#                  ex_coupon_date: datetime,
+#                  is_in_arrears: bool = False, # da implementare convexity adjustments
+#                  fixing_days: int = None,
+#                  ):
+#         super().__init__(payment_date, nominal, accrual_start_date, accrual_end_date, ref_period_start, ref_period_end, ex_coupon_date)
+#         self._day_counter = daycounter
+#         self._fixing_days = fixing_days 
+#         self._index = index
+#         self._gearing = gearing
+#         self._spread = spread
+#         self._is_in_arrears = is_in_arrears
 
-    @property
-    def day_counter(self):
-        return self._day_counter
+#     @property
+#     def day_counter(self):
+#         return self._day_counter
     
-    @property
-    def fixing_days(self):
-        if self._fixing_days is None:
-            if self.index is not None:
-                return self.index.fixing_days
-            else:
-                return 0
+#     @property
+#     def fixing_days(self):
+#         if self._fixing_days is None:
+#             if self.index is not None:
+#                 return self.index.fixing_days
+#             else:
+#                 return 0
     
-    @property
-    def index(self):
-        return self._index
+#     @property
+#     def index(self):
+#         return self._index
     
-    @property
-    def fixing_adjusted(self):
-        if self.is_in_arrears:
-            #if the pay date is equal to the index estimation end date
-            # there is no convexity; in all other cases in principle an
-            # adjustment has to be applied
-            raise ValueError("Convexity Adjustment non ancora implementato") 
-        else:
-            return self.index.fixing(self.fixing_date)
+#     @property
+#     def fixing_adjusted(self):
+#         if self.is_in_arrears:
+#             #if the pay date is equal to the index estimation end date
+#             # there is no convexity; in all other cases in principle an
+#             # adjustment has to be applied
+#             raise ValueError("Convexity Adjustment non ancora implementato") 
+#         else:
+#             return self.index.fixing(self.fixing_date)
             
-    @property
-    def is_in_arrears(self):
-        return self._is_in_arrears
+#     @property
+#     def is_in_arrears(self):
+#         return self._is_in_arrears
     
-    @property
-    def fixing_date(self):
-        if self.is_in_arrears:
-            ref_date = self.accrual_end_date 
-        else:
-            ref_date = self.accrual_start_date
-        return ref_date + timedelta(self.fixing_days)
+#     @property
+#     def fixing_date(self):
+#         if self.is_in_arrears:
+#             ref_date = self.accrual_end_date 
+#         else:
+#             ref_date = self.accrual_start_date
+#         return ref_date + timedelta(self.fixing_days)
     
-    @property
-    def swaplet_rate(self):
-        return self._gearing * self.fixing_adjusted + self._spread
+#     @property
+#     def swaplet_rate(self):
+#         return self._gearing * self.fixing_adjusted + self._spread
         
-    @property
-    def accrual_period(self):
-        return (self.accrual_end_date - self.accrual_start_date).days / 365
+#     @property
+#     def accrual_period(self):
+#         return (self.accrual_end_date - self.accrual_start_date).days / 365
 
-    @property
-    def amount(self)-> float:
-        ''' 
-        Da testare funzione _rate.compoundFactor (versione overloaded su file interestrate.hpp)
-        '''
-        return self.nominal * self.swaplet_rate * self.accrual_period
+#     @property
+#     def amount(self)-> float:
+#         ''' 
+#         Da testare funzione _rate.compoundFactor (versione overloaded su file interestrate.hpp)
+#         '''
+#         return self.nominal * self.swaplet_rate * self.accrual_period
     
-    @property
-    def accruedAmount(self, d: datetime):
-        if d <= self.accrual_start_date or d > self._payment_date:
-            return 0
-        else:
-            return self.nominal * (self._rate.compoundFactor(self.accrual_start_date,
-                                                            min(d, self.accrual_end_date),
-                                                            self.ref_period_start,
-                                                            self.ref_period_end
-                                                            ) - 1)
+#     @property
+#     def accruedAmount(self, d: datetime):
+#         if d <= self.accrual_start_date or d > self._payment_date:
+#             return 0
+#         else:
+#             return self.nominal * (self._rate.compoundFactor(self.accrual_start_date,
+#                                                             min(d, self.accrual_end_date),
+#                                                             self.ref_period_start,
+#                                                             self.ref_period_end
+#                                                             ) - 1)
         
-class FloatingRateLeg:
-    def __init__(self,
-                 schedule: Schedule,
-                 notionals: list[float],
-                 gearings: list[float],
-                 spreads: list[float],
-                 index: CurveRateIndex
-                 ) -> None:
-        self._schedule = schedule
-        self._notionals = notionals
-        self._gearings = gearings
-        self._spreads = spreads 
-        self._index = index
+# class FloatingRateLeg:
+#     def __init__(self,
+#                  schedule: Schedule,
+#                  notionals: list[float],
+#                  gearings: list[float],
+#                  spreads: list[float],
+#                  index: CurveRateIndex
+#                  ) -> None:
+#         self._schedule = schedule
+#         self._notionals = notionals
+#         self._gearings = gearings
+#         self._spreads = spreads 
+#         self._index = index
   
-    @property
-    def schedule(self):
-        return self._schedule
+#     @property
+#     def schedule(self):
+#         return self._schedule
     
-    @property
-    def notionals(self):
-        return self._notionals
+#     @property
+#     def notionals(self):
+#         return self._notionals
 
 
-    def leg_flows(self):
-        ''' 
-        Define the leg as a list of FixedCoupon objects
-        TBD: definire bene tutti gli accrual 
-        '''
-        leg = []
-        for i in range(1, len(self._schedule)):
-            period_start_date = self._schedule[i-1]
-            payment_date = self.schedule[i]
-            nom = self._notionals[i-1]
+#     def leg_flows(self):
+#         ''' 
+#         Define the leg as a list of FixedCoupon objects
+#         TBD: definire bene tutti gli accrual 
+#         '''
+#         leg = []
+#         for i in range(1, len(self._schedule)):
+#             period_start_date = self._schedule[i-1]
+#             payment_date = self.schedule[i]
+#             nom = self._notionals[i-1]
 
-            leg.append(FloatingCoupon(payment_date,
-                                    nom,
-                                    period_start_date,
-                                    payment_date,
-                                    self._index,
-                                    self._gearings[i-1],
-                                    self._spreads[i-1],
-                                    period_start_date,
-                                    payment_date,
-                                    None,
-                                    period_start_date
-                                    )
-                            )
-        return leg
+#             leg.append(FloatingCoupon(payment_date,
+#                                     nom,
+#                                     period_start_date,
+#                                     payment_date,
+#                                     self._index,
+#                                     self._gearings[i-1],
+#                                     self._spreads[i-1],
+#                                     period_start_date,
+#                                     payment_date,
+#                                     None,
+#                                     period_start_date
+#                                     )
+#                             )
+#         return leg
     
-    def display_flows(self):
-        flows = self.leg_flows()
-        period_start_date = []
-        period_end_date = []
-        for i in range(len(flows)):
-            period_start_date.append(self.schedule[i])
-            period_end_date.append(self.schedule[i+1])
-        amounts = [flows[t].amount for t in range(len(flows))]
-        leg_overview = {}
-        leg_overview['period_start_date'] = period_start_date
-        leg_overview['period_end_date'] = period_end_date
-        leg_overview['notional'] = self.notionals
-        return 
+#     def display_flows(self):
+#         flows = self.leg_flows()
+#         period_start_date = []
+#         period_end_date = []
+#         for i in range(len(flows)):
+#             period_start_date.append(self.schedule[i])
+#             period_end_date.append(self.schedule[i+1])
+#         amounts = [flows[t].amount for t in range(len(flows))]
+#         leg_overview = {}
+#         leg_overview['period_start_date'] = period_start_date
+#         leg_overview['period_end_date'] = period_end_date
+#         leg_overview['notional'] = self.notionals
+#         return 
 
-    def npv(self, discount_curve, evaluation_date: datetime):
-        ''' 
-        Calcola l'npv data una gamba qualsiasi
-        Si potrebbe eliminare la data di valutazione in quanto recuperabile come prima data delle curva in input
-        '''
-        Settings.evaluation_date = evaluation_date
-        if len(self.leg_flows()) == 0:
-            return 0
-        npv = 0
-        for i in range(0, len(self.leg_flows())):
-            cf = self.leg_flows()[i]
-            if not cf.has_occurred(evaluation_date):
-                dt = cf.date - evaluation_date
-                dt_year_fraction = dt.days/365
-                npv += cf.amount * discount_curve.discount(dt_year_fraction)
-        return npv
+#     def npv(self, discount_curve, evaluation_date: datetime):
+#         ''' 
+#         Calcola l'npv data una gamba qualsiasi
+#         Si potrebbe eliminare la data di valutazione in quanto recuperabile come prima data delle curva in input
+#         '''
+#         Settings.evaluation_date = evaluation_date
+#         if len(self.leg_flows()) == 0:
+#             return 0
+#         npv = 0
+#         for i in range(0, len(self.leg_flows())):
+#             cf = self.leg_flows()[i]
+#             if not cf.has_occurred(evaluation_date):
+#                 dt = cf.date - evaluation_date
+#                 dt_year_fraction = dt.days/365
+#                 npv += cf.amount * discount_curve.discount(dt_year_fraction)
+#         return npv
         
-class FloatingCoupon_dev(Coupon):
+class FloatingCoupon(Coupon):
     ''' 
     Concrete Floating Coupon object. 
     '''
@@ -234,6 +235,67 @@ class FloatingCoupon_dev(Coupon):
         return (self.accrual_end_date - self.accrual_start_date).days / 365
 
     
+    def display(self, 
+                disc_curve,
+                term_structure,
+                evaluation_date):
+        Settings.evaluation_date = evaluation_date
+
+        if not self.has_occurred(evaluation_date):
+            dt = self.date - evaluation_date
+            dt_year_fraction = dt.days/365
+            disc_factor = (disc_curve.discount(dt_year_fraction)).numpy()
+            coupon_npv = self.amount(term_structure) * disc_factor
+            amount = self.amount(term_structure)
+            fixing = self.index.fixing(self.fixing_date,
+                                        term_structure)
+            is_expired = False
+        else:
+            coupon_npv = 0
+            disc_factor = 0
+            fixing = 0
+            amount = 0
+            is_expired = True
+
+        coupon_display = pd.DataFrame([self.ref_period_start,
+                                        self.ref_period_end,
+                                        self.date,
+                                        self._nominal,
+                                        self.fixing_date,
+                                        self._fixing_days,
+                                        self._index.name,
+                                        fixing,
+                                        self.accrual_period,
+                                        self.is_in_arrears,
+                                        self._gearing,
+                                        self._spread,
+                                        self._day_counter,
+                                        amount,
+                                        disc_factor,
+                                        coupon_npv,
+                                        is_expired
+                                        ]).T
+
+        coupon_display.columns = ['start_period',
+                                'end_period',
+                                'payment_date',
+                                'notional',
+                                'fixing_date',
+                                'fixing_days',
+                                'index',
+                                'fixing',
+                                'accrual',
+                                'in_arrears',
+                                'gearing',
+                                'spread',
+                                'day_counter',
+                                'amount',
+                                'discount_factor',
+                                'coupon_pv',
+                                'is_expired'
+                                ]
+        return coupon_display
+    
     def amount(self, term_structure)-> float:
         ''' 
         Da testare funzione _rate.compoundFactor (versione overloaded su file interestrate.hpp)
@@ -241,8 +303,7 @@ class FloatingCoupon_dev(Coupon):
         a = self.nominal * (self._gearing * self.index.fixing(self.fixing_date,
                                                                  term_structure) + self._spread) * self.accrual_period
         return a
-    
-    
+       
     def accruedAmount(self, d: datetime):
         if d <= self.accrual_start_date or d > self._payment_date:
             return 0
@@ -253,13 +314,13 @@ class FloatingCoupon_dev(Coupon):
                                                             self.ref_period_end
                                                             ) - 1)
         
-class FloatingRateLeg_dev:
+class FloatingRateLeg:
     def __init__(self,
                  schedule: Schedule,
                  notionals: list[float],
                  gearings: list[float],
                  spreads: list[float],
-                 index: CurveRateIndex
+                 index: IborIndex
                  ) -> None:
         self._schedule = schedule
         self._notionals = notionals
@@ -287,7 +348,7 @@ class FloatingRateLeg_dev:
             payment_date = self.schedule[i]
             nom = self._notionals[i-1]
 
-            leg.append(FloatingCoupon_dev(payment_date,
+            leg.append(FloatingCoupon(payment_date,
                                     nom,
                                     period_start_date,
                                     payment_date,
@@ -302,19 +363,18 @@ class FloatingRateLeg_dev:
                             )
         return leg
     
-    def display_flows(self):
+    def display_flows(self,
+                      disc_curve,
+                      term_structure,
+                      evaluation_date):
         flows = self.leg_flows()
-        period_start_date = []
-        period_end_date = []
+        leg_display = pd.DataFrame()
         for i in range(len(flows)):
-            period_start_date.append(self.schedule[i])
-            period_end_date.append(self.schedule[i+1])
-        amounts = [flows[t].amount for t in range(len(flows))]
-        leg_overview = {}
-        leg_overview['period_start_date'] = period_start_date
-        leg_overview['period_end_date'] = period_end_date
-        leg_overview['notional'] = self.notionals
-        return 
+            coupon_flow = flows[i].display(disc_curve,
+                                           term_structure,
+                                           evaluation_date)
+            leg_display = pd.concat([leg_display, coupon_flow], axis = 0)
+        return leg_display
 
     def npv(self,
             discount_curve,
