e3add5b968bdca58011b8886b9af4876463ccce1
Andrea Carapelli <carapelliandrea@gmail.com>
2024-07-23 15:22:16 +0200

    add swap example

--- tquant/pricers/__init__.py
+++ tquant/pricers/__init__.py
@@ -1,6 +1,6 @@
 from .fixedflow import *
 from .floatingflow import *
-# from .swapdiscounting import *
+from .swapdiscounting import *
 # from .blackcapfloor import *
 # from .cdsdiscounting import *
 from .fradiscounting import *
