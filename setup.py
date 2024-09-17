987ade325d1f2d9cc2c87a4030ae8388bb63c471
Andrea Carapelli <carapelliandrea@gmail.com>
2024-09-17 13:46:46 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tensorquant",  # Nome del pacchetto
-    version="0.0.3",  # Versione del pacchetto
-    packages=find_packages(),  # Trova automaticamente tutti i pacchetti e sottopacchetti
+    name="tensorquant",  
+    version="0.0.3",  
+    packages=find_packages(),  
     install_requires=[],  # Dipendenze richieste, puoi aggiungere pacchetti necessari qui
     description="TensorFlow-Python financial library",
-    long_description=open('README.md').read(),  # Usa il file README per la descrizione lunga
-    long_description_content_type='text/markdown',  # Specifica il formato della descrizione lunga
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown',  
     author="Andrea Carapelli",
     author_email="carapelliandrea@email.com",
-    url="https://github.com/andrea220/tQuant",  # URL del progetto (puoi usare GitHub o un sito personale)
-    classifiers=[  # Classificatori per descrivere il pacchetto
+    url="https://github.com/andrea220/tQuant",  
+    classifiers=[  
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # Specifica la licenza
+        "License :: OSI Approved :: MIT License",  
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',  # Versione minima di Python richiesta
+    python_requires='>=3.10',  
     test_suite='tests'
 )
