983ddcb34f0e848ea145318ad4dbc2ba1ace4ab3
Andrea Carapelli <carapelliandrea@gmail.com>
2024-09-17 13:49:44 +0200

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tensorquant",  
-    version="0.0.3",  
+    version="0.0.4",  
     packages=find_packages(),  
     install_requires=[],  # Dipendenze richieste, puoi aggiungere pacchetti necessari qui
     description="TensorFlow-Python financial library",
