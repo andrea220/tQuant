eb63c1a13b1c89244d3e1a8d976e182993dc0e55
Andrea Carapelli <carapelliandrea@gmail.com>
2026-03-11 15:04:01 +0100

    updated setup file

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tensorquant",  
-    version="0.0.6",  
-    packages=find_packages(),  
+    name="tensorquant",
+    version="1.0.0",
+    packages=find_packages(),
     install_requires=[
-        'tensorflow>=2.0',
-        'tensorflow-probability>=0.20',
-        'pandas',
-        'python-dateutil',
-    ],  
+        "tensorflow>=2.0",
+        "python-dateutil==2.9.0.post0",
+        "pandas==2.2.2",
+    ],
     description="TensorFlow-Python financial library",
-    long_description=open('README.md', encoding="utf8").read(),  
-    long_description_content_type='text/markdown',  
+    long_description=open("README.md", encoding="utf8").read(),
+    long_description_content_type="text/markdown",
     author="Andrea Carapelli",
     author_email="carapelliandrea@email.com",
-    url="https://github.com/andrea220/tQuant",  
-    classifiers=[  
+    url="https://github.com/andrea220/tQuant",
+    classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",  
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',  
-    test_suite='tests'
+    python_requires=">=3.10",
+    test_suite="tests",
 )
